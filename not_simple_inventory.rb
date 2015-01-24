f1db17bee8aab2acd832348ffde6da5c6fa0c2b2
Dave Bennett <dmbennett@anderson.edu>
2015-01-24 17:06:19 -0500

    Fixed validation errors, I think bulletproof now

--- not_simple_inventory.rb
+++ not_simple_inventory.rb
@@ -9,16 +9,16 @@
 inventory = {"apple" => 1, "banana" => 10, "orange" => 20, "concord grapes" => 10}
 
 def print_in_table(inventory)
-    # the following figures out the maximum length of the item string
+  # the following figures out the maximum length of the item string
   item_spacing = 0
   inventory.each do |item, quantity|
     item_spacing = item.length
-      if item_spacing < item.length
-        item_spacing = item.length
-      end
+    if item_spacing < item.length
+      item_spacing = item.length
+    end
   end
 
-    #the following prints it using the spacing calculated above
+  #the following prints it using the spacing calculated above
   puts "ITEM:".ljust(item_spacing+1) + "| QUANTITY:"
   inventory = inventory.sort_by { |item, quantity| item}
   inventory.each do |item, quantity|
@@ -44,19 +44,17 @@ def update_item_and_quantity (inventory, item, quantity)
   end
 end
 
-def validate_item (item)
-  valid_entry = false
-  while valid_entry == false
-    item = item
-    if item =~ /\d/
-      puts 'Your item name cannot have a number in it.'
-      puts 'What is the name of your item? '
-      item = gets.chomp.downcase
-    else
-      valid_entry = true
-    end
+def contains_number? (variable)
+  if variable =~ /\d/
+    return true
+  else
+    return false
   end
 end
+
+
+
+
 ##################################################################################################################
 exit = false
 #begin the actual program loop, exit when exit = true
@@ -64,51 +62,62 @@ while exit == false do
 
   puts %q{Welcome to Kellyn and Dave's Emporium
   Please select an item from the following menu
-  1. Print - to print a table of our items and what we have in stock
+  1. Print  - to print a table of our items and what we have in stock
   2. Update - to add a new item or update the quantity of an item already in stock
   3. Remove - to remove an item
-  4. Exit - will exit the program
+  4. Exit   - will exit the program
 What is your selection? Type: print, update, or remove: }
   menu_selection = gets.chomp.downcase
 
   if menu_selection == "print" || menu_selection == "1. print" || menu_selection =="1" || menu_selection =="1."
     #table-ish
-      print_in_table(inventory)
+    print_in_table(inventory)
 
   elsif menu_selection == "update" || menu_selection =="2. add" || menu_selection =="2" || menu_selection =="2."
-    #adding items
-      puts "What item would like to update or add?"
-        item = gets.chomp.downcase
-        validate_item(item)
-      puts "Please enter the total quantity: "
-        quantity = gets.chomp.to_i
-      update_item_and_quantity(inventory, item, quantity)
-      print_in_table(inventory)
+    #adding or updating items
+    puts "What item would like to update or add?"
+    item = gets.chomp.downcase
+    while contains_number?(item)
+      puts 'Your item name cannot have a number in it. Please re-enter the item name'
+      item = gets.chomp.downcase
+    end
+
+    puts "Please enter the total quantity: "
+    quantity = Integer(gets.chomp) rescue nil
+    until quantity.is_a?(Fixnum) do
+      puts 'You must enter a whole number: '
+      quantity = Integer(gets) rescue nil
+    end
+
+    update_item_and_quantity(inventory, item, quantity)
+    print_in_table(inventory)
 
   elsif menu_selection == "remove" || menu_selection =="3. remove" || menu_selection =="3" || menu_selection =="3."
     #remove
-      print_in_table(inventory)
-      puts "What item would you like to remove?"
-        item_to_remove = gets.chomp.downcase
-        inventory.delete(item_to_remove)
-      print_in_table(inventory)
+    print_in_table(inventory)
+    puts "What item would you like to remove?"
+    item_to_remove = gets.chomp.downcase
+    inventory.delete(item_to_remove)
+    print_in_table(inventory)
+  elsif menu_selection == "exit" || menu_selection =="4. exit" || menu_selection =="4" || menu_selection =="4."
+    break
 
   else
     #did they have a typo? then ask them to check again
     puts "Please check your selection, you typed: #{menu_selection}?"
-    puts "#{menu_selection} is not a valid entry"
+    puts "\"#{menu_selection.capitalize}\" is not a valid entry"
   end
 
   puts "Would you like to continue updating the inventory? [y/n]: "
   exit = gets.chomp.downcase
 
   #the condition for ending our while that ends our program
-    if exit == "y" || exit=="yes"
-      exit = false
-    elsif exit == "n" || exit=="no"
-      exit = true
-    else
-      puts "You may have made a typo, returning you to the main menu."
-      exit = false
-    end
+  if exit == "y" || exit=="yes"
+    exit = false
+  elsif exit == "n" || exit=="no"
+    exit = true
+  else
+    puts "You may have made a typo, returning you to the main menu."
+    exit = false
+  end
 end
